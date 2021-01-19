a3af742328af2c3eb01a414dba0f82f0587ba637
werserk <74610069+werserk@users.noreply.github.com>
2021-01-19 17:58:39 +0300

    Delete setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-from os.path import join, dirname
-
-setup(
-    name='Typical Dungeon',
-    packages=find_packages(),
-    long_description=open(join(dirname(__file__), 'README.txt')).read(),
-)
